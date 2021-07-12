1460940a4da2cd3afe439171318ef39e5e08a2b0
hongguanghe <46474783+hongguanghe@users.noreply.github.com>
2021-07-12 10:30:33 -0600

    Update Batch.cs

--- InventoryManagement/Model/Batch.cs
+++ InventoryManagement/Model/Batch.cs
@@ -7,5 +7,11 @@ namespace InventoryManagement.Model
 {
     public class Batch
     {
+        public string Id { get; set; }
+        public int Quantities { get; set; }
+        public double Cost { get; set; }
+        public DateTime PurchasedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Location { get; set; }
     }
 }
