b9c71e0dd89e7a17f670c84598f63e2a8a6d5279
hongguanghe <46474783+hongguanghe@users.noreply.github.com>
2021-08-26 20:21:56 -0600

    Clean up

--- InventoryManagement/Controllers/InventoryController.cs
+++ InventoryManagement/Controllers/InventoryController.cs
@@ -46,8 +46,8 @@ namespace InventoryManagement.Controllers
             return _mapper.Map<ProductResponse>(productDto);
         }
         
-        [HttpPatch ("products/product/{id}", Name ="Update Product")]
-        public async Task<ActionResult> UpdateProduct(ProductDTO productDto)
+        [HttpPut ("products/product/{id}", Name ="Update Product")]
+        public async Task<ActionResult> UpdateProduct([FromBody]ProductDTO productDto)
         {
             if (!await _productService.ProductExistsById(productDto.ProductId))
             {
@@ -97,7 +97,7 @@ namespace InventoryManagement.Controllers
         [HttpPost("products/product/create", Name ="Create Product")]
         public async Task<ActionResult> CreateProduct(ProductDTO productDto)
         {
-            if (!await _productService.ProductExistsByName(productDto.Name))
+            if (await _productService.ProductExistsByName(productDto.Name))
             {
                 return BadRequest();
             }
@@ -114,10 +114,6 @@ namespace InventoryManagement.Controllers
         [HttpPost("batches/batch/create", Name = "Create One Batch")]
         public async Task<ActionResult> CreateBatch(BatchDTO batchDto)
         {
-            if (!await _batchService.BatchExistsById(batchDto.BatchId))
-            {
-                return BadRequest();
-            }
             await _batchService.CreateBatch(batchDto);
             return Ok();        
         }
@@ -133,8 +129,8 @@ namespace InventoryManagement.Controllers
             return Ok();
         }
                 
-        [HttpPatch("batches/batch/{id}", Name = "Update One Batch")]
-        public async Task<ActionResult> UpdateBatch(BatchDTO batchDto)
+        [HttpPut("batches/batch/{id}", Name = "Update One Batch")]
+        public async Task<ActionResult> UpdateBatch([FromBody]BatchDTO batchDto)
         {
             if (!await _batchService.BatchExistsById(batchDto.BatchId))
             {
@@ -150,151 +146,5 @@ namespace InventoryManagement.Controllers
             var batchesResult = await _batchService.GetAllAssociatedBatches(productId);
             return batchesResult.ToList().Select(x => _mapper.Map<BatchResponse>(x)).ToList();
         }
-
-        // [HttpPost("products/demo", Name = "Add Demo")]
-        // public async Task<ActionResult> LoadDemoData()
-        // {
-        //     var demoProducts = new List<ProductDTO>();
-        //
-        //     // ------------------Product 1 -------------------
-        //     var product1 = new ProductDTO
-        //     {
-        //         Name = "Pods Spring Meadow 96 Ct, Laundry Detergent Pacs",
-        //         Brand = "Tide",
-        //         Category = "Detergents",
-        //         Price = 21.44,
-        //         OnSale = true,
-        //         Location = "A1",
-        //     };
-        //
-        //     var batchesList1 = new List<BatchDTO>();
-        //     var batch1List1 = new BatchDTO
-        //     {
-        //         Quantities = 100,
-        //         Cost = 9.88,
-        //         Manufacturer = "Tide Inc.",
-        //         PurchasedDate = new System.DateTime(),
-        //         ExpirationDate = new System.DateTime(2025, 5, 1),
-        //     };
-        //
-        //     var batch2List1 = new BatchDTO
-        //     {
-        //         Quantities = 100,
-        //         Cost = 12.55,
-        //         Manufacturer = "Tide Inc.",
-        //         PurchasedDate = new System.DateTime(),
-        //         ExpirationDate = new System.DateTime(2025, 5, 6),
-        //     };
-        //
-        //     var batch3List1 = new BatchDTO
-        //     {
-        //         Quantities = 300,
-        //         Cost = 18.55,
-        //         Manufacturer = "Tide Inc.",
-        //         PurchasedDate = new System.DateTime(),
-        //         ExpirationDate = new System.DateTime(2025, 5, 25),
-        //     };
-        //     batchesList1.Add(batch1List1);
-        //     batchesList1.Add(batch2List1);
-        //     batchesList1.Add(batch3List1);
-        //     product1.Batches = batchesList1;
-        //     demoProducts.Add(product1);
-        //
-        //     // ------------------Product 2 -------------------
-        //     var product2 = new ProductDTO
-        //     {
-        //         Name = "Plastic Set of(4) 12 Qt.Storage Boxes Blush Pink",
-        //         Brand = "Sterilite",
-        //         Category = "Kitchen",
-        //         Price = 8.00,
-        //         OnSale = false,
-        //         Location = "C5",
-        //     };
-        //
-        //     var batchesList2 = new List<BatchDTO>();
-        //     var batch1List2 = new BatchDTO
-        //     {
-        //         Quantities = 100,
-        //         Cost = 9.88,
-        //         Manufacturer = "Sterilite Company",
-        //         PurchasedDate = new System.DateTime(2015, 5, 1),
-        //         ExpirationDate = new System.DateTime(2025, 8, 1),
-        //     };
-        //
-        //     var batch2List2 = new BatchDTO
-        //     {
-        //         Cost = 12.55,
-        //         Manufacturer = "Sterilite Company",
-        //         PurchasedDate = new System.DateTime(2016, 12, 8),
-        //         ExpirationDate = new System.DateTime(2028, 8, 8),
-        //     };
-        //
-        //     batchesList2.Add(batch1List2);
-        //     batchesList2.Add(batch2List2);
-        //     product2.Batches = batchesList2;
-        //     demoProducts.Add(product2);
-        //
-        //     // ------------------Product 3 -------------------
-        //     var product3 = new ProductDTO
-        //     {
-        //         Name = "Comfort Wireless Combo Keyboard and Mouse",
-        //         Brand = "Logitech",
-        //         Category = "Electronics",
-        //         Price = 48.88,
-        //         OnSale = true,
-        //         Location = "E2",
-        //     };
-        //
-        //     var batchesList3 = new List<BatchDTO>();
-        //     var batch1List3 = new BatchDTO
-        //     {
-        //         Quantities = 30,
-        //         Cost = 38.00,
-        //         Manufacturer = "Logitech Inc",
-        //         PurchasedDate = new System.DateTime(2008, 5, 1),
-        //         ExpirationDate = new System.DateTime(2012, 8, 1),
-        //     };
-        //
-        //     var batch2List3 = new BatchDTO
-        //     {
-        //         Quantities = 10,
-        //         Cost = 35.00,
-        //         Manufacturer = "Logitech Inc",
-        //         PurchasedDate = new System.DateTime(2012, 12, 8),
-        //         ExpirationDate = new System.DateTime(2022, 8, 8),
-        //     };
-        //
-        //     var batch3List3 = new BatchDTO
-        //     {
-        //         Quantities = 5,
-        //         Cost = 32.66,
-        //         Manufacturer = "Logitech Inc",
-        //         PurchasedDate = new System.DateTime(2018, 1, 8),
-        //         ExpirationDate = new System.DateTime(2020, 3, 28),
-        //     };
-        //
-        //     var batch4List3 = new BatchDTO
-        //     {
-        //         Quantities = 5,
-        //         Cost = 49.55,
-        //         Manufacturer = "Logitech Inc",
-        //         PurchasedDate = new System.DateTime(2020, 7, 5),
-        //         ExpirationDate = new System.DateTime(2028, 8, 15),
-        //     };
-        //
-        //     batchesList2.Add(batch1List3);
-        //     batchesList2.Add(batch2List3);
-        //     batchesList2.Add(batch3List3);
-        //     batchesList2.Add(batch4List3);
-        //     product2.Batches = batchesList3;
-        //     demoProducts.Add(product3);
-        //
-        //     foreach (var p in demoProducts)
-        //     {
-        //         await _productService.CreateProduct(p);
-        //     }
-        //
-        //     return Ok();
-        // }
     }
 }
